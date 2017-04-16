efd8dadd7c5339592dbdf1983bcfeb2be812ec06
Ethan Allen <ethancallen@gmail.com>
2017-04-16 19:23:21 -0400

    updated webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 var webpack = require("webpack");
 var path = require("path");
-
+ 
 var DEV = path.resolve(__dirname, "dev");
-var OUTPUT = path.resolve(__dirname,"output");
+var OUTPUT = path.resolve(__dirname, "output");
+ 
 var config = {
-	entry: DEV + "/index.js",
-	output: {
-		path: OUTPUT,
-		filename: "template.js"
-	},
-	module: {
-		loaders: [{
-			include: DEV,
-			loader: "babel-loader",
-		}]
-	}
+	  entry: DEV + "/index.jsx",
+	    output: {
+		        path: OUTPUT,
+			    filename: "myCode.js"
+				      },
+	      module: {
+		          loaders: [{
+				          include: DEV,
+					          loader: "babel-loader",
+						      }]
+				     }
 };
-
+ 
 module.exports = config;
